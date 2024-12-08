d4cc6484057f1187435d51ec7c50336ead90af79
Momodu_Ope <opeyemi.momodu@student.kdg.be>
2024-12-08 15:55:02 +0100

    changes to the analytics script.

--- queryGenerator/analytics.sql
+++ queryGenerator/analytics.sql
@@ -54,18 +54,18 @@ ORDER BY
 LIMIT 100;
 
 -- Get most popular games
-SELECT g.game_name, SUM(pgs.total_games_played) as total_matches
+SELECT g.name, SUM(pgs.total_games_played) as total_matches
 FROM games g
 JOIN player_game_stats pgs ON g.game_id = pgs.game_id
-GROUP BY g.game_name
+GROUP BY g.name
 ORDER BY total_matches DESC;
 
 -- Get player activity by country
-SELECT location, COUNT(*) as player_count,
+SELECT country, COUNT(*) as player_count,
        AVG(total_games_played) as avg_games_played
 FROM players p
 JOIN player_game_stats pgs ON p.player_id = pgs.player_id
-GROUP BY location
+GROUP BY country
 ORDER BY player_count DESC;
 
 
