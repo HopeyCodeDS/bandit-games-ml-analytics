76d96b0b009c1e1ee9e6ec995125785d5211c81b
Momodu_Ope <opeyemi.momodu@student.kdg.be>
2025-01-02 18:37:14 +0100

    Update player_statistics_API_new.py to allow requests from all origins.

--- GameAnalytics/player_statistics_API_new.py
+++ GameAnalytics/player_statistics_API_new.py
@@ -39,7 +39,7 @@ origins = [
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
